9fc9afd4e2c8e485201d3c60ad47fc45fdafc30f
ZeTheNerd <56926789+ZeTheNerd@users.noreply.github.com>
2025-03-06 17:16:22 +0000

    feat(RestController): send sum operation to kafka

--- rest/src/main/java/com/challenge/rest/controllers/RestAPIController.java
+++ rest/src/main/java/com/challenge/rest/controllers/RestAPIController.java
@@ -1,19 +1,44 @@
 package com.challenge.rest.controllers;
 
 import java.math.BigDecimal;
+import java.util.UUID;
 
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
+import org.springframework.kafka.requestreply.ReplyingKafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.challenge.rest.pojos.CalculatorOperation;
+import com.challenge.rest.pojos.CalculatorTask;
+
 @RestController
 public class RestAPIController {
+
+    @Value("${kafka.topic.calculator-request-topic}")
+    private String requestTopic;
+
+    @Autowired
+    private ReplyingKafkaTemplate<String, CalculatorTask, BigDecimal> replyingTemplate;
 
     // GET /sum?a=<BigDecimal>&b=<BigDecimal>
     // Endpoint for sum operation
     @GetMapping(value="sum", produces={MediaType.APPLICATION_JSON_VALUE})
     public BigDecimal getSum(@RequestParam BigDecimal a, @RequestParam BigDecimal b) {
+        // create unique ID for this request
+        String id = UUID.randomUUID().toString();
+
+        // create message to be sent to calculator via kafka
+        CalculatorTask sum = new CalculatorTask(CalculatorOperation.SUM, a, b);
+        ProducerRecord<String, CalculatorTask> record = new ProducerRecord<String,CalculatorTask>(requestTopic, id, sum);
+
+        // send mesage to broker
+        replyingTemplate.sendAndReceive(record);
+
+
         return a.add(b);
     }
 
