34e79466ea6ffc82d225cca7669380311572523b
ZeTheNerd <56926789+ZeTheNerd@users.noreply.github.com>
2025-03-07 17:33:38 +0000

    chore(KafkaConfig.java): remove unused imports)

--- rest/src/main/java/com/challenge/rest/KafkaConfig.java
+++ rest/src/main/java/com/challenge/rest/KafkaConfig.java
@@ -1,27 +1,17 @@
 package com.challenge.rest;
 
 import java.math.BigDecimal;
-import java.util.Map;
 
 import org.apache.kafka.clients.admin.NewTopic;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringDeserializer;
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.config.TopicBuilder;
-import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
 import org.springframework.kafka.requestreply.ReplyingKafkaTemplate;
-import org.springframework.kafka.support.serializer.JsonDeserializer;
-import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import com.challenge.rest.pojos.CalculatorTask;
 
