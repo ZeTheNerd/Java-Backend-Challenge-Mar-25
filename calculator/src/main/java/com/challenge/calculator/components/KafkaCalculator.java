5674922eb3c4cc056f530a5df70aacc0b1767fd2
ZeTheNerd <56926789+ZeTheNerd@users.noreply.github.com>
2025-03-07 17:31:55 +0000

    feat(logging): configure logging and MDC across modules

--- calculator/src/main/java/com/challenge/calculator/components/KafkaCalculator.java
+++ calculator/src/main/java/com/challenge/calculator/components/KafkaCalculator.java
@@ -3,6 +3,9 @@ package com.challenge.calculator.components;
 import java.math.BigDecimal;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Component;
@@ -11,6 +14,8 @@ import com.challenge.calculator.pojos.CalculatorTask;
 
 @Component
 public class KafkaCalculator {
+    Logger logger = LoggerFactory.getLogger(KafkaCalculator.class);
+    private String mdcKey = "requestID";
     
     // method responsible for consuming the request topic,
     // computing the operation and sending the result to the result topic
@@ -19,7 +24,10 @@ public class KafkaCalculator {
     public BigDecimal processRequest(ConsumerRecord<String, CalculatorTask> record) {
         // get request ID from the record's key
         String requestID = record.key();
+        MDC.put(mdcKey, requestID);
+
         // get requested operation from the record's value
+        logger.info("Received new calculator task for processing");
         CalculatorTask task = record.value();
 
         // execute the corresponding operation
@@ -28,19 +36,25 @@ public class KafkaCalculator {
         BigDecimal result = null;
         switch (task.getOperation()) {
             case SUM:
-                result = first.add(second);
+            result = first.add(second);
+            logger.info("Executed SUM a=" + first + " b=" + second + " result=" +result);
                 break;
             case SUBTRACTION:
                 result = first.subtract(second);
+                logger.info("Executed SUBTRACTION a=" + first + " b=" + second + " result=" +result);
                 break;
             case MULTIPLICATION:
                 result = first.multiply(second);
+                logger.info("Executed MULTIPLICATION a=" + first + " b=" + second + " result=" +result);
                 break;
             case DIVISION:
                 result = first.divide(second);
+                logger.info("Executed DIVISION a=" + first + " b=" + second + " result=" +result);
                 break;
         }
 
+        logger.info("Publishing result in Kafka");
+        MDC.remove(mdcKey);
         // send result to result topic
         return result;
     }
--- rest/src/main/java/com/challenge/rest/controllers/RestAPIController.java
+++ rest/src/main/java/com/challenge/rest/controllers/RestAPIController.java
@@ -8,6 +8,9 @@ import java.util.concurrent.TimeoutException;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
@@ -20,8 +23,12 @@ import org.springframework.web.bind.annotation.RestController;
 import com.challenge.rest.pojos.CalculatorOperation;
 import com.challenge.rest.pojos.CalculatorTask;
 
+import jakarta.servlet.http.HttpServletResponse;
+
 @RestController
 public class RestAPIController {
+    Logger logger = LoggerFactory.getLogger(RestAPIController.class);
+    private String mdcKey = "requestID";
 
     @Value("${kafka.topic.calculator-request-topic}")
     private String requestTopic;
@@ -32,76 +39,112 @@ public class RestAPIController {
     // GET /sum?a=<BigDecimal>&b=<BigDecimal>
     // Endpoint for sum operation
     @GetMapping(value="sum", produces={MediaType.APPLICATION_JSON_VALUE})
-    public String getSum(@RequestParam BigDecimal a, @RequestParam BigDecimal b) throws InterruptedException, ExecutionException, TimeoutException {
+    public String getSum(@RequestParam BigDecimal a, @RequestParam BigDecimal b, HttpServletResponse response)
+    throws InterruptedException, ExecutionException, TimeoutException {
         // create unique ID for this request
         String id = UUID.randomUUID().toString();
+        MDC.put(mdcKey, id);
 
         // create message to be sent to calculator via kafka
+        logger.info("Received request for SUM a=" + a + " b=" + b);
         CalculatorTask sum = new CalculatorTask(CalculatorOperation.SUM, a, b);
         ProducerRecord<String, CalculatorTask> record = new ProducerRecord<String,CalculatorTask>(requestTopic, id, sum);
 
         // send mesage to request topic
+        logger.info("Sending request for SUM a=" + a + " b=" + b + " to kafka");
         RequestReplyFuture<String, CalculatorTask, BigDecimal> future = replyingTemplate.sendAndReceive(record);
         // await for reply in results topic
         ConsumerRecord<String, BigDecimal> result = future.get(5, TimeUnit.SECONDS);
+        logger.info("Received result for SUM a=" + a + " b=" + b + " from kafka: " + result.value());
 
+        // add request ID to response headers
+        response.addHeader("X-Request-Id", id);
+
+        MDC.remove(mdcKey);
         return "{\"result\": " + result.value() + "}";
     }
 
     // GET /subtraction?a=<BigDecimal>&b=<BigDecimal>
     // Endpoint for subtraction operation
     @GetMapping(value="subtraction", produces=MediaType.APPLICATION_JSON_VALUE)
-    public String getSubtraction(@RequestParam BigDecimal a, @RequestParam BigDecimal b) throws InterruptedException, ExecutionException, TimeoutException {
+    public String getSubtraction(@RequestParam BigDecimal a, @RequestParam BigDecimal b, HttpServletResponse response)
+    throws InterruptedException, ExecutionException, TimeoutException {
         // create unique ID for this request
         String id = UUID.randomUUID().toString();
+        MDC.put(mdcKey, id);
 
         // create message to be sent to calculator via kafka
+        logger.info("Received request for SUBTRACTION a=" + a + " b=" + b);
         CalculatorTask subtraction = new CalculatorTask(CalculatorOperation.SUBTRACTION, a, b);
         ProducerRecord<String, CalculatorTask> record = new ProducerRecord<String,CalculatorTask>(requestTopic, id, subtraction);
 
         // send mesage to request topic
+        logger.info("Sending request for SUBTRACTION a=" + a + " b=" + b + " to kafka");
         RequestReplyFuture<String, CalculatorTask, BigDecimal> future = replyingTemplate.sendAndReceive(record);
         // await for reply in results topic
         ConsumerRecord<String, BigDecimal> result = future.get(5, TimeUnit.SECONDS);
+        logger.info("Received result for SUBTRACTION a=" + a + " b=" + b + " from kafka: " + result.value());
 
+        // add request ID to response headers
+        response.addHeader("X-Request-Id", id);
+
+        MDC.remove(mdcKey);
         return "{\"result\": " + result.value() + "}";
     }
 
     // GET /multiplication?a=<BigDecimal>&b=<BigDecimal>
     // Endpoint for multiplication operation
     @GetMapping(value="multiplication", produces=MediaType.APPLICATION_JSON_VALUE)
-    public String getMultiplication(@RequestParam BigDecimal a, @RequestParam BigDecimal b) throws InterruptedException, ExecutionException, TimeoutException {
+    public String getMultiplication(@RequestParam BigDecimal a, @RequestParam BigDecimal b, HttpServletResponse response)
+    throws InterruptedException, ExecutionException, TimeoutException {
         // create unique ID for this request
         String id = UUID.randomUUID().toString();
+        MDC.put(mdcKey, id);
 
         // create message to be sent to calculator via kafka
+        logger.info("Received request for MULTIPLICATION a=" + a + " b=" + b);
         CalculatorTask multiplication = new CalculatorTask(CalculatorOperation.MULTIPLICATION, a, b);
         ProducerRecord<String, CalculatorTask> record = new ProducerRecord<String,CalculatorTask>(requestTopic, id, multiplication);
 
         // send mesage to request topic
+        logger.info("Sending request for MULTIPLICATION a=" + a + " b=" + b + " to kafka");
         RequestReplyFuture<String, CalculatorTask, BigDecimal> future = replyingTemplate.sendAndReceive(record);
         // await for reply in results topic
         ConsumerRecord<String, BigDecimal> result = future.get(5, TimeUnit.SECONDS);
+        logger.info("Received result for MULTIPLICATION a=" + a + " b=" + b + " from kafka: " + result.value());
 
+        // add request ID to response headers
+        response.addHeader("X-Request-Id", id);
+
+        MDC.remove(mdcKey);
         return "{\"result\": " + result.value() + "}";
     }
 
     // GET /division?a=<BigDecimal>&b=<BigDecimal>
     // Endpoint for multiplication operation
     @GetMapping(value="division", produces=MediaType.APPLICATION_JSON_VALUE)
-    public String getDivision(@RequestParam BigDecimal a, @RequestParam BigDecimal b) throws InterruptedException, ExecutionException, TimeoutException {
+    public String getDivision(@RequestParam BigDecimal a, @RequestParam BigDecimal b, HttpServletResponse response)
+    throws InterruptedException, ExecutionException, TimeoutException {
         // create unique ID for this request
         String id = UUID.randomUUID().toString();
+        MDC.put(mdcKey, id);
 
         // create message to be sent to calculator via kafka
+        logger.info("Received request for DIVISION a=" + a + " b=" + b);
         CalculatorTask division = new CalculatorTask(CalculatorOperation.DIVISION, a, b);
         ProducerRecord<String, CalculatorTask> record = new ProducerRecord<String,CalculatorTask>(requestTopic, id, division);
 
         // send mesage to request topic
+        logger.info("Sending request for DIVISION a=" + a + " b=" + b + " to kafka");
         RequestReplyFuture<String, CalculatorTask, BigDecimal> future = replyingTemplate.sendAndReceive(record);
         // await for reply in results topic
         ConsumerRecord<String, BigDecimal> result = future.get(5, TimeUnit.SECONDS);
+        logger.info("Received result for DIVISION a=" + a + " b=" + b + " from kafka: " + result.value());
 
+        // add request ID to response headers
+        response.addHeader("X-Request-Id", id);
+
+        MDC.remove(mdcKey);
         return "{\"result\": " + result.value() + "}";
     }
 }
