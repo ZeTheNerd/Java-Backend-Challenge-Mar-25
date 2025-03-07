cfe6a5df96f090d322352c4f2ec7cc2e20dd88fd
ZeTheNerd <56926789+ZeTheNerd@users.noreply.github.com>
2025-03-07 00:19:55 +0000

    feat(KafkaCalculator): handle all operations

--- calculator/src/main/java/com/challenge/calculator/components/KafkaCalculator.java
+++ calculator/src/main/java/com/challenge/calculator/components/KafkaCalculator.java
@@ -2,6 +2,7 @@ package com.challenge.calculator.components;
 
 import java.math.BigDecimal;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Component;
@@ -11,11 +12,36 @@ import com.challenge.calculator.pojos.CalculatorTask;
 @Component
 public class KafkaCalculator {
     
-    //@KafkaListener(topics="${kafka.topic.calculator-request-topic}")
-    @KafkaListener(topics="calculator-request")
-    @SendTo("calculator-result")
-    public BigDecimal processRequest(CalculatorTask task) {
-        BigDecimal result = task.getFirst().add(task.getSecond());
+    // method responsible for consuming the request topic,
+    // computing the operation and sending the result to the result topic
+    @KafkaListener(topics="${kafka.topic.calculator-request-topic}")
+    @SendTo("${kafka.topic.calculator-reply-topic}")
+    public BigDecimal processRequest(ConsumerRecord<String, CalculatorTask> record) {
+        // get request ID from the record's key
+        String requestID = record.key();
+        // get requested operation from the record's value
+        CalculatorTask task = record.value();
+
+        // execute the corresponding operation
+        BigDecimal first = task.getFirst();
+        BigDecimal second = task.getSecond();
+        BigDecimal result = null;
+        switch (task.getOperation()) {
+            case SUM:
+                result = first.add(second);
+                break;
+            case SUBTRACTION:
+                result = first.subtract(second);
+                break;
+            case MULTIPLICATION:
+                result = first.multiply(second);
+                break;
+            case DIVISION:
+                result = first.divide(second);
+                break;
+        }
+
+        // send result to result topic
         return result;
     }
 }
